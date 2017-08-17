48aa59f706cd51d7704d7613bafe730d4f3c35f7
mm@example.com <mm@example.com>
2017-08-17 19:45:27 +0800

    03-删除任务

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 		.module('todoApp', [])
 		.controller('TodoController', ['$scope', function ($scope) {
             var vm=$scope;
-			// 数据：
+			// 1.展示数据：
 			// name 表示任务名称
 			// isCompleted 表示任务是否完成的状态
 			// id 唯一标识
@@ -16,10 +16,11 @@
 				{ id: 3, name: '烫头', isCompleted: false },
 			];
 
-			//添加任务
+			//2.添加任务
 			vm.todoList=todoList;
 			vm.todoName="";
 			vm.add=function(){
+				//名字为空格，直接结束
 				if(vm.todoName.trim()===""){
 					return;
 				}
@@ -27,8 +28,23 @@
 				id=todoList.length===0?1:todoList.length+1;
 				todoList.push({id:id,name:vm.todoName,isCompleted:false});
 				vm.todoName="";
-			}
+			};
 
+            //3.删除任务
+			vm.del=function(id){
+				for(var i=0;i<todoList.length;i++){
+					if(todoList[i].id===id){
+						todoList.splice(i,1);
+						break;
+					}
+				}
+			};
+
+			/*//4.修改任务
+            vm.edit=function(id){
+                var editId=-1;
+
+			}*/
 
 
 		}]);
